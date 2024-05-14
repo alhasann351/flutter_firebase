d6b53dbd3550dda5a17b6c7a23d903608e952be2
alhasann351 <alhasann352@gmail.com>
2024-05-14 16:38:31 +0600

    image-upload

--- lib/ui/uploaded_image.dart
+++ lib/ui/uploaded_image.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/ui/widgets/round_button.dart';
-
+import 'package:image_picker/image_picker.dart';
 import '../utils/utils.dart';
 import 'auth/login_screen.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 class UploadedImage extends StatefulWidget {
   const UploadedImage({super.key});
@@ -13,8 +16,27 @@ class UploadedImage extends StatefulWidget {
 }
 
 class _UploadedImageState extends State<UploadedImage> {
+  bool loading = false;
 
   final _auth = FirebaseAuth.instance;
+
+  firebase_storage.FirebaseStorage storage = firebase_storage.FirebaseStorage.instance;
+  final databaseRef = FirebaseDatabase.instance.ref('Posts');
+
+  File? _image;
+  final picker = ImagePicker();
+
+  Future getGalleryImage() async{
+    final pickedImage = await picker.pickImage(source: ImageSource.gallery, imageQuality: 80);
+    
+    setState(() {
+      if(pickedImage != null){
+        _image = File(pickedImage.path);
+      }else{
+        Utils().showToast('No image picked');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,7 +82,7 @@ class _UploadedImageState extends State<UploadedImage> {
             Center(
               child: InkWell(
                 onTap: (){
-
+                  getGalleryImage();
                 },
                 child: Container(
                   height: 200,
@@ -70,13 +92,44 @@ class _UploadedImageState extends State<UploadedImage> {
                       color: Colors.black,
                     )
                   ),
-                  child: const Center(child: Icon(Icons.image_outlined)),
+                  child: _image != null ? Image.file(_image!.absolute) : const Center(child: Icon(Icons.image_outlined)),
                 ),
               ),
             ),
             const SizedBox(height: 20,),
-            RoundButton(title: 'Upload', onTap: (){
+            RoundButton(title: 'Upload', loading: loading, onTap: () {
 
+              setState(() {
+                loading = true;
+              });
+
+              firebase_storage.Reference ref = firebase_storage.FirebaseStorage.instance.ref('/photos/${DateTime.now().millisecondsSinceEpoch}');
+              firebase_storage.UploadTask uploadTask = ref.putFile(_image!.absolute);
+
+              Future.value(uploadTask).then((value) async{
+                var newUrl = await ref.getDownloadURL();
+
+                databaseRef.child('id').set({
+                  'id' : '123456',
+                  'title' : newUrl.toString(),
+                }).then((value){
+                  setState(() {
+                    loading = false;
+                  });
+                  Utils().showToast('Upload success');
+
+                }).onError((error, stackTrace){
+                  setState(() {
+                    loading = false;
+                  });
+                  Utils().showToast(error.toString());
+                });
+              }).onError((error, stackTrace){
+                Utils().showToast(error.toString());
+                setState(() {
+                  loading = false;
+                });
+              });
             },),
           ],
         ),
